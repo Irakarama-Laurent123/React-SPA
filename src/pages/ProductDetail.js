e75c5506d76d908a32d83ff07d56512ec02657e8
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-09-06 11:09:22 +0200

    Extracted Route params

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import {useParams} from 'react-router-dom'
 
 function ProductDetail() {
+  const params = useParams()
+
+  console.log(params.productId)
+
   return (
-    <div>
+    <section>
       <h1>Product Detail</h1>
-    </div>
+      <p>{params.productId}</p>
+    </section>
   )
 }
 
