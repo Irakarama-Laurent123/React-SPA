30f822879d65d6c367f318ccd7b99c4776f68966
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-09-06 17:47:14 +0200

    Added a comments navigation

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,31 +1,36 @@
-import { useParams, Route } from 'react-router-dom'
-import React from 'react'
+import { useParams, Route, Link } from "react-router-dom";
+import React from "react";
 
-import Comments from '../components/comments/Comments'
-import HighlightedQuote from '../components/quotes/HighlightedQuote'
+import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
 
 const DUMMY_QUOTES = [
-  {id: 'q1', author: 'Laurent', text: '"It ain\'t bragging if it\'s true" '},
-  {id: 'q2', author: 'Zest', text:'"Work, Live, and Create"'}
-]
+  { id: "q1", author: "Laurent", text: "\"It ain't bragging if it's true\" " },
+  { id: "q2", author: "Zest", text: '"Work, Live, and Create"' },
+];
 
 const QuoteDetail = () => {
-  const params = useParams()
+  const params = useParams();
 
-  const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId)
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
 
-  if (!quote){
-    return <p>No Quote Found!</p>
+  if (!quote) {
+    return <p>No Quote Found!</p>;
   }
 
   return (
     <div>
-      <HighlightedQuote text={quote.text} author={quote.author}/>
+      <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={`/quotes/${params.quoteId}` } exact>
+      <div className="centered">
+        <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>Load Comments</Link>
+      </div>
+      </Route>
       <Route path={`/quotes/${params.quoteId}/comments`}>
-        <Comments/>
+        <Comments />
       </Route>
     </div>
-  )
-}
+  );
+};
 
-export default QuoteDetail
+export default QuoteDetail;
